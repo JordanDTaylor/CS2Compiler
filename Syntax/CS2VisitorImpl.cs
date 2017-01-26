b2e85a8fc10c7e489bb261031b94b9f5fe288b04
Ming Slogar <Ming Slogar>
2017-01-25 18:39:31 -0700

    Changed to use other Visit methods.

--- Syntax/CS2VisitorImpl.cs
+++ Syntax/CS2VisitorImpl.cs
@@ -14,7 +14,7 @@ namespace Syntax
 
 		public override Result VisitDeclaration([NotNull] DeclarationContext context)
 		{
-			string type = context.GetChild(0).GetChild(0).ToString();
+			string type = VisitType(context.GetChild<TypeContext>(0)).ToString();
 
 			for (int i = 1; i < context.ChildCount; i++)
 			{
@@ -40,7 +40,7 @@ namespace Syntax
 				index++;
 			}
 
-			string type = context.GetChild(index++).GetChild(0).ToString();
+			string type = VisitType(context.GetChild<TypeContext>(index++)).ToString();
 			string name = context.GetChild(index++).ToString();
 
 			functionHolder.Add(name, context);
@@ -116,6 +116,6 @@ namespace Syntax
 			}
 
 			return default(Result);
-		}	
+		}
 	}
 }
