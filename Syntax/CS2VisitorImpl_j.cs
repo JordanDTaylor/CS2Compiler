d33b5d815292a18001eed41c4b6935ba335a4333
Jordan Taylor <jordandtaylor@gmail.com>
2017-01-26 17:25:18 -0700

    Finish my implemenation

--- Syntax/CS2VisitorImpl_j.cs
+++ Syntax/CS2VisitorImpl_j.cs
@@ -1,4 +1,8 @@
-﻿namespace Syntax
+﻿using System;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace Syntax
 {
     // ReSharper disable once InconsistentNaming
 	partial class CS2VisitorImpl
@@ -18,27 +22,68 @@
             return Visit(context.GetChild(0));
         }
 
+        /// <summary>
+        /// Returns the enum UnaryOperator
+        /// </summary>
         public override object VisitPre_unary_operator(CS2Parser.Pre_unary_operatorContext context)
         {
-            return base.VisitPre_unary_operator(context);
+            if (context.GetType() == typeof(RuleContext))
+                return Visit(context.post_unary_operator());
+
+            return UnaryOperator.Negate;
         }
 
+        /// <summary>
+        /// Returns the enum UnaryOperator
+        /// </summary>
         public override object VisitPost_unary_operator(CS2Parser.Post_unary_operatorContext context)
         {
-            return base.VisitPost_unary_operator(context);
+            if (context.OP_DEC() != null)
+                return UnaryOperator.Decrement;
+
+            if (context.OP_INC() != null)
+                return UnaryOperator.Increment;
+
+            throw new Exception("Post_unary_operator context dosn't contain a post unary operator...");
         }
 
         public override object VisitType(CS2Parser.TypeContext context)
         {
-            return context.GetText();
+            if (context.TYPE_DOUBLE() != null)
+                return TypeToken.Double;
+            if (context.TYPE_INT() != null)
+                return TypeToken.Int;
+            if (context.TYPE_STRING() != null)
+                return TypeToken.String;
+            if (context.TYPE_VOID() != null)
+                return TypeToken.Void;
+            throw new Exception("TypeContext context dosn't contain a type token...");
+
         }
 
         public override object VisitArrayType(CS2Parser.ArrayTypeContext context)
         {
-            return base.VisitArrayType(context);
+            if (context.TYPE_DOUBLE() != null)
+                return TypeToken.DoubleArray;
+            if (context.TYPE_INT() != null)
+                return TypeToken.IntArray;
+            if (context.TYPE_STRING() != null)
+                return TypeToken.StringArray;
+            throw new Exception("ArrayTypeContext context dosn't contain an array type token...");
+
         }
     }
 
+    internal enum TypeToken
+    {
+        Double,
+        Void,
+        String,
+        Int,
+        StringArray,
+        IntArray,
+        DoubleArray
+    }
     internal enum UnaryOperator
     {
         Increment,
